af14e4f0614d7fd276f6fa7f7dbdcd02a8de72f1
Kumar Gaurav <kgaurav@Kumars-MBP.Dlink>
2019-12-22 18:22:11 +0530

    setup pages done

--- src/renderer/components/AppLanding.js
+++ src/renderer/components/AppLanding.js
@@ -1,124 +1,66 @@
-import { Sidebar } from "./Sidebar";
-import { PanelCloudItems } from "./PanelCloudItems";
-import { PanelMonitor } from "./PanelMonitor";
-
 export class AppLanding {
   constructor(container) {
-    this._container = $(container);
+    document.title = "Glimpse : Setup";
+    $(container).html(/*html*/ `
+        <div class="ui bottom fixed menu">
+          <div class="ui container labeled icon button attached green flat">
+              <i class="right arrow icon"></i>
+              I'm done Configuring, load the app now
+          </div>
+        </div>
+        <div class="ui container" style="padding-bottom:40px;">
+          <div class="ui basic segment">
+            <h3 class="ui dividing header">Terms & Conditions</h3>
+            <p>
+              Glimpse uses aws node sdk to cmmunicate with your aws cloudaccount(s) to
+            </p>
+            <ol>
+              <li>[read only] see through all configured aws accounts</li>
+              <li>[read only] list various resources like ec2, asg, ecs and more.</li>
+              <li>[read only] fetch available Cloudwatch metrics for the resouces </li>
+            </ol>
+            <div class="ui checkbox ">
+              <input type="checkbox" name="example">
+              <label>Seems fine :-)</label>
+            </div>
+          </div>
+          <div class="ui basic segment">
+            <h3 class="ui dividing header">Accounts</h3>
+            <div class="ui cards">
+              <div class="card">
+                <div class="content">
+                  <div class="header">
+                    <div class="ui checkbox ">
+                      <input type="checkbox" name="example">
+                      <label>Stage</label>
+                    </div>
+                  </div>
+                  <div class="description">
+                    <p>Default Zone = us-east-1</p>
+                    <p>Content Type = json</p>
+                  </div>
+                </div>
+              </div>
+              <div class="card">
+                <div class="content">
+                  <div class="header">
+                    <div class="ui checkbox ">
+                      <input type="checkbox" name="example">
+                      <label>Prod</label>
+                    </div>
+                  </div>
+                  <div class="description">
+                    <p>Default Zone = us-east-1</p>
+                    <p>Content Type = json</p>
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
 
-    this._mainLayoutConfig = {
-      name: "layout-home",
-      panels: [
-        {
-          type: "left",
-          resizable: true,
-          size: 200,
-          minSize: 150,
-          hidden: true
-        },
-        {
-          type: "main",
-          resizable: true,
-        },
-        // {
-        //   type: "right",
-        //   resizable: true,
-        //   size: "50%",
-        //   // minSize: 200
-        // },
-        {
-          type: "bottom",
-          size: 25,
-          toolbar: {
-            name: "toolbar",
-            style: "padding:0px 5px",
-            items: [
-              // { type: "button", id: "item7", caption: "Glimpse" },
-              // { type: "break", id: "break2" },
-              {
-                type: "html",
-                id: "item2",
-                html: item =>
-                  `<span>Built with &hearts; for AWS&trade; cloud heros</span>`
-              },
-              { type: "spacer" },
-              { type: "button", id: "item3", caption: "Report Bug" },
-              { type: "break", id: "break1" },
-              { type: "button", id: "item6", caption: "Preferences" }
-            ],
-            onClick: function(event) {
-              switch (event.target) {
-                case "item6":
-                  // w2ui["layout-home"].toggle("right");
-                  break;
-              }
-            }
-          }
-        },
-        {
-          type: "top",
-          resizable: false,
-          size: 35,
-          toolbar: {
-            style: "padding: 2px; background: #e1e1e1; -webkit-app-region: drag",
-            items: [
-              {
-                type: "button",
-                id: "logs",
-                caption: "Logs",
-                img: "icon-page"
-              },
-              { type: "break", id: "break1" },
-              {
-                type: "menu",
-                id: "charts",
-                caption: "Charts",
-                img: "icon-folder",
-                items: [
-                  { text: "CPU Utilisation" },
-                  { text: "Network In" },
-                  { text: "Network Out" }
-                ]
-              },
-              { type: "spacer" },
-              {
-                type: 'html', id: 'item1',
-                html: function (item) {
-                  var html =`<input style="margin-right:10px;" size="75" placeholder="<CPU Utilization> chart for <Website> application from <Stage> account"/>`;
-                  return html;
-                }
-              }
-            ]
-          }
-        }
-      ]
-    };
 
-    this._w2uiInstance = this._container.w2layout(this._mainLayoutConfig);
-
-    this._leftContainer = this._w2uiInstance.el("left");
-    this._mainContainer = this._w2uiInstance.el("main");
-    this._rightContainer = this._w2uiInstance.el("right");
-    this._topContainer = this._w2uiInstance.el("top");
-    this._bottomContainer = this._w2uiInstance.el("bottom");
-
-    this._w2uiInstance.on(
-      "resize",
-      function(e) {
-        e.done(
-          function() {
-            $(this._leftContainer).trigger("containerResize");
-            $(this._mainContainer).trigger("containerResize");
-            $(this._rightContainer).trigger("containerResize");
-            $(this._topContainer).trigger("containerResize");
-            $(this._bottomContainer).trigger("containerResize");
-          }.bind(this)
-        );
-      }.bind(this)
-    );
-
-    // new Sidebar(this._leftContainer);
-    new PanelCloudItems(this._mainContainer);
-    // new PanelMonitor(this._rightContainer);
+      `);
   }
 }
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,35 +1,20 @@
-// Global css styles
-import "golden-layout/src/css/goldenlayout-base.css";
-import "golden-layout/src/css/goldenlayout-light-theme.css";
-import "bootstrap/dist/css/bootstrap";
-
-// Some more css styles
-import "./lib/w2ui-1.5.rc1.min.css";
-import "./lib/app.style.css";
-
 // Dirty hack untill we crack webpack configuration
 import "./lib/global-scripts";
 
-require('bootstrap');
-import AWS from "aws-sdk";
+/////////// ** CSS ** ///////////
+// Global css styles
+// import "golden-layout/src/css/goldenlayout-base.css";
+// import "golden-layout/src/css/goldenlayout-light-theme.css";
+import "semantic-ui/dist/semantic.min.css";
+
+// Some more css styles
+// import "./lib/w2ui-1.5.rc1.min.css";
+import "./lib/app.style.css";
+
+/////////// ** JS ** ///////////
+import 'semantic-ui/dist/semantic'
 
 import { AppLanding } from "./components/AppLanding";
-// import { AppAuth } from "./components/AppAuth";
-
-// import { AppAuth } from "./components/AppAuth";
-
-// var ec2 = new AWS.S3({
-//   region: "us-east-1",
-//   credentials: new AWS.SharedIniFileCredentials({ profile: "root" })
-// });
-// var params = { DryRun: false };
-
-// console.log(ec2);
-// ec2.listBuckets({}, (err, data) =>
-//   err ? rej(err) : res(data.Reservations)
-//   console.log(err)
-  // console.log(data)
-// );
+import { SettingUp } from "./components/SettingUp";
 
 new AppLanding("#app");
-// new AppAuth('#app');
